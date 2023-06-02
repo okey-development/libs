fa99bbb34bb8e2fec307aa4a5d9a44906d61ad85
P4elll1991 <p4elll1991@gmail.com>
2023-06-02 20:19:02 +0500

    убран тест

--- service/token.go
+++ service/token.go
@@ -91,7 +91,6 @@ const (
 
 func getUserByUUID(uuid string) (*User, error) {
 	userId, _ := GetStringInt64(uuid)
-	Info("getUserByUUID userId %d", userId)
 	if userId == 0 {
 		user, err := GetUserByUUID(uuid)
 		if err != nil {
